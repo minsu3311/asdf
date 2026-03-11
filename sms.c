6d798b45285d8198f5d5b79d6f32b840b3148bfd
minsu3311 <chssal1792@gmail.com>
2026-03-11 23:41:02 +0900

    sms

---
+++ sms.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main(void)
+{
+  printf(" ___  _ __ ___   ___ \n");
+  printf("/ __|| '_ ` _ \\ / __| \n");
+  printf("\\__ \\| | | | | |\\__ \\ \n");
+  printf("|___/|_| |_| |_||___/ \n"); 
+  
+
+}  
